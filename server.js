29aa21359ca2d3cd8c34113d89e3f38d8c901c57
juanmagdev <me@juanmagd.dev>
2023-08-15 20:04:18 +0200

    API works now for silver and gold

--- server.js
+++ server.js
@@ -1,74 +1,24 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
 const express = require('express');
-const {google} = require('googleapis');
-const credentials = require('./key.json');
+const { getCoinPrices } = require('./utils/coinUtils');
+
 const app = express();
 
 const PORT = 3000;
 
-const auth = new google.auth.GoogleAuth({
-    credentials,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-});
-const sheets = google.sheets({ version: 'v4', auth });
-
-// Función para obtener los precios de las monedas
-async function getCoinPrices() {
-    try {
-        const response = await axios.get('https://www.dracmametales.com/precio-de-la-plata');
-
-        const $ = cheerio.load(response.data);
-        const coinData = [];
-
-        $('tr.product-container').each((index, element) => {
-            const coinName = $(element).find('.nombre a').text().trim();
-            const coinWeight = $(element).find('.peso').text().trim();
-            const coinPrice = $(element).find('.venta-precio .PricesalesPrice').text().trim();
-
-            coinData.push({
-                name: coinName,
-                weight: coinWeight,
-                price: coinPrice
-            });
-        });
-
-        console.log(coinData);
-        // await insertPricesToGoogleSheet(coinData);
-        
-        return coinData;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return null;
-    }
-}
-
-async function insertPricesToGoogleSheet(prices){
-    const spreadSheetId = '1bmxqGYllWcQdFlr_fDXM1zT_kAM3ZOSagBjsPEXO7Bk';
-
-    const values = prices.map(price => [price.name, price.weight, price.price]);
-
-    console.log(values);
-
-    await sheets.spreadsheets.values.append({
-        auth: auth,
-        spreadsheetId: spreadSheetId,
-        range: 'A2', // Rango de celdas donde se insertarán los datos
-        valueInputOption: 'RAW',
-        resource: {
-            values: values
+// Dracma Store
+app.get('/getPrices/Dracma/:type', async (req, res) => {
+    const type = req.params.type.toLowerCase();
+    if(type === 'plata' || type === 'oro'){
+        const prices = await getCoinPrices('Dracma', type);
+        if (prices) {
+            res.json(prices);
+        } else {
+            res.status(500).json({ error: 'Error fetching prices' });
         }
-    });
-}
-
-// Ruta para obtener los precios de las monedas
-app.get('/getPrices', async (req, res) => {
-    const prices = await getCoinPrices();
-    if (prices) {
-        res.json(prices);
-    } else {
-        res.status(500).json({ error: 'Error fetching prices' });
+    }else{
+        res.status(404).json({ error: 'Type not found' });
     }
+
 });
 
 app.listen(PORT, () => {
---
+++ utils/coinUtils.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function getCoinPrices(comerce, type) {
+    try {
+        const coinData = [];
+
+        if (comerce === 'Dracma') {
+            if (type === 'plata') {
+                const response = await axios.get('https://www.dracmametales.com/precio-de-la-plata',);
+
+                const $ = cheerio.load(response.data);
+
+                $('tr.product-container').each((index, element) => {
+                    const coinName = $(element).find('.nombre a').text().trim();
+                    const coinWeight = $(element).find('.peso').text().trim();
+                    const coinPrice = $(element).find('.venta-precio .PricesalesPrice').text().trim();
+
+                    coinData.push({
+                        name: coinName,
+                        weight: coinWeight,
+                        price: coinPrice
+                    });
+                });
+            }else if(type === 'oro'){
+                const response = await axios.get('https://www.dracmametales.com/precio-del-oro',);
+
+                const $ = cheerio.load(response.data);
+
+                $('tr.product-container').each((index, element) => {
+                    const coinName = $(element).find('.nombre a').text().trim();
+                    const coinWeight = $(element).find('.peso').text().trim();
+                    const coinPrice = $(element).find('.venta-precio .PricesalesPrice').text().trim();
+
+                    coinData.push({
+                        name: coinName,
+                        weight: coinWeight,
+                        price: coinPrice
+                    });
+                });
+            }
+        }else if(comerce === 'Numismatica'){
+            //etc
+        }
+
+        console.log(coinData);
+
+        return coinData;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
+}
+
+module.exports = {
+    getCoinPrices
+};
